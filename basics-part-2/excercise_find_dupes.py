5324864fcd191936d272719adc3bc621c8e5e43d
Samantha Genevay <samanthagenevay@gmail.com>
2023-06-26 12:54:00 +0100

    Python exercise duplicate checker

---
+++ basics-part-2/excercise_find_dupes.py
@@ -0,0 +1,26 @@
+# Check for duplicates in the list
+my_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+def duplicate_checker(a_list):
+    unique_list = []
+    dupe_list = []
+    for item in a_list:
+        if item not in unique_list:
+            unique_list.append(item)
+        else:
+            dupe_list.append(item)
+            print(f"This {item} is a dupe!")
+    print(dupe_list)
+
+duplicate_checker(my_list)
+
+
+def duplicate_checker2(a_list):
+    dupe_list = []
+    for item in a_list:
+        if a_list.count(item) > 1 and item not in dupe_list:
+            dupe_list.append(item)
+    print(dupe_list)
+
+duplicate_checker2(my_list)
+
