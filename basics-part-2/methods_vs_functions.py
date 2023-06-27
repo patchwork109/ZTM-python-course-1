1aa176de95f9ab6b6a2d946ae145b06cc8a1776d
Samantha Genevay <samanthagenevay@gmail.com>
2023-06-27 11:51:00 +0100

    Python methods vs. functions

---
+++ basics-part-2/methods_vs_functions.py
@@ -0,0 +1,15 @@
+# built-in functions examples
+# called by stating the function name and adding parentheses
+print()
+min()
+max()
+input()
+
+# built-in methods examples
+# format -> the_thing (e.g. string, list, tuple, etc).method()
+# whatever's to the left of the dot owns the method
+
+'hello'.capitalize()
+'hello'.count()
+'hello'.upper()
+
