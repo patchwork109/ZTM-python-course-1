3d6c3d5c67cec66e69e3088a358a65c5df3f938a
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-07 16:46:00 +0100

    Python tricky counter excercise

---
+++ basics-part-2/exercise_tricky_counter.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+counter = 0
+for num in my_list:
+    counter = counter + num
+    print(f"The current count is {counter}")
+print(f"The final count is {counter}")
