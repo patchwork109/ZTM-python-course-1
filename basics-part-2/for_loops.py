22ea070c4065e5ad9c5d0b389cec9d3c18d13092
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-08 13:05:00 +0100

    Python for loops

---
+++ basics-part-2/for_loops.py
@@ -0,0 +1,11 @@
+# format -> for variable in iterable:
+
+for item in [1,2,3,4,5]:
+    print(item)
+
+for character in 'Sam':
+    print(character)
+
+for item in (1,2,3,4,5):
+    for letter in ['a', 'b', 'c', 'd']:
+        print(item, letter)
