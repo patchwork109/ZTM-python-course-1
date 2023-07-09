b58cf26477cafb186f2d0ddcc257d65b52b9e5a7
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-09 12:23:00 +0100

    Python logical operators

---
+++ basics-part-2/logical_operators.py
@@ -0,0 +1,15 @@
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+# and
+# or
+# not
+
+print(4 < 5) # -> True
+print(4 == 5) # -> False
+print(1 <= 0) # -> False
+print(0 != 1) # -> True
+print(not(True)) # -> False
