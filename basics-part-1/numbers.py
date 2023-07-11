74150fbdcba9b85e142d2077501ce0d8d8216acc
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-11 10:55:16 -0700

    Python numbers

---
+++ basics-part-1/numbers.py
@@ -0,0 +1,13 @@
+print(type(2 + 4))
+print(2 + 4)
+print(type(2 - 4))
+print(2 - 4)
+print(type(2 * 4))
+print(2 * 4)
+print(type(2 / 4))
+print(2 / 4)
+
+print(2.1 + 4.9)
+print(2 ** 4) # ** -> to the power of (e.g. 2 to the power of 4)
+print(10 // 4) # // -> divides and returns an integer instead of a float
+print(10 % 4) # modulo -> the remainder after dividing (e.g. 10 divided by 4 has a remainder of 2)
