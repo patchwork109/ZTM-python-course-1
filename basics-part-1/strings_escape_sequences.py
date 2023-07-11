12050147af34c33b26d5266698ef5282a063e3d1
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-11 11:53:37 -0700

    Python strings escape methods

---
+++ basics-part-1/strings_escape_sequences.py
@@ -0,0 +1,9 @@
+weather = "It's sunny" # option to use double quotes b/c of single quote use
+weather2 = "\t It\'s \"kind of\" sunny \n hope you have a good day!"
+
+# \ let's the interpreter know to read the next character as a string
+# \t inserts a tab
+# \n inserts a new line
+
+print(weather)
+print(weather2)
