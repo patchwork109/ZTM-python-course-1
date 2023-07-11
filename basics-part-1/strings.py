7568b322969c4ad279d67569dee901d6dff4858e
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-11 11:48:28 -0700

    Python strings

---
+++ basics-part-1/strings.py
@@ -0,0 +1,16 @@
+print(type("hi there, hello!"))
+
+username = 'supercoder'
+password = 'supersecret'
+long_string = '''
+WWWWW
+ 0 0 
+\___/
+'''
+
+print(username, password, long_string)
+
+first_name = 'Sam'
+last_name = 'Genevay'
+full_name = first_name + ' ' + last_name
+print(full_name)
