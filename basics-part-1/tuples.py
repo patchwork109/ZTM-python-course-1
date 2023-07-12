dfda13116d8d8f75e4c2d78436a904cd6edc329f
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-11 18:08:55 -0700

    Python tuples

---
+++ basics-part-1/tuples.py
@@ -0,0 +1,9 @@
+# immutable lists
+
+my_tuple = (1, 2, 3, 4, 5)
+
+# can use similar list methods
+print(5 in my_tuple) # -> True
+print(my_tuple[0]) # -> 1
+print(my_tuple.count(2)) # -> 1
+print(len(my_tuple)) # -> 5
